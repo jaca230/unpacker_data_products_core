37915ad392391b3e30d1a8eff05a6ff698e78dc2
jaca230 <jaca230@uky.edu>
2025-07-15 19:58:36 -0400

    first commit

---
+++ include/LinkDef.h
@@ -0,0 +1,11 @@
+#if defined(__ROOTCLING__)
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+// Data products
+#pragma link C++ class dataProducts::DataProduct+;
+#pragma link C++ class std::vector<dataProducts::DataProduct>+;
+
+#endif
---
+++ src/data_products/DataProduct.cpp
@@ -0,0 +1,9 @@
+#include "data_products/DataProduct.h"
+
+using namespace dataProducts;
+
+DataProduct::DataProduct() {}
+    
+DataProduct::~DataProduct() {}
+
+void DataProduct::Show() const {}
