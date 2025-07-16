e7bd66a86bd5034d233b9dbc666eae599ce94621
jaca230 <jaca230@uky.edu>
2025-07-15 21:42:04 -0400

    changed bytestream to not make a copy of data

--- include/data_products/common/ByteStream.h
+++ include/data_products/common/ByteStream.h
@@ -2,28 +2,23 @@
 #define DATA_PRODUCTS_BYTESTREAM_H
 
 #include "DataProduct.h"
-#include <vector>
 #include <cstdint>
+#include <cstddef>
 
 namespace dataProducts {
 
-    class ByteStream : public DataProduct {
-    public:
-        ByteStream();
-        explicit ByteStream(std::vector<uint8_t> data);
-        ~ByteStream() override;
+class ByteStream : public DataProduct {
+public:
+    ByteStream();
+    ~ByteStream() override;
 
-        const std::vector<uint8_t>& buffer() const;
-        std::vector<uint8_t>& buffer();
-        void SetBuffer(std::vector<uint8_t> data);
+    const uint8_t* data = nullptr;
+    size_t size = 0;
 
-        void Show() const override;
+    void Show() const override;
 
-    private:
-        std::vector<uint8_t> buffer_;
-
-        ClassDefOverride(ByteStream, 1);
-    };
+    ClassDefOverride(ByteStream, 2);
+};
 
 } // namespace dataProducts
 
--- src/data_products/common/ByteStream.cpp
+++ src/data_products/common/ByteStream.cpp
@@ -7,36 +7,20 @@ ClassImp(dataProducts::ByteStream)
 
 namespace dataProducts {
 
-    ByteStream::ByteStream() = default;
+ByteStream::ByteStream() = default;
+ByteStream::~ByteStream() = default;
 
-    ByteStream::ByteStream(std::vector<uint8_t> data)
-        : buffer_(std::move(data)) {}
-
-    ByteStream::~ByteStream() = default;
-
-    const std::vector<uint8_t>& ByteStream::buffer() const {
-        return buffer_;
-    }
-
-    std::vector<uint8_t>& ByteStream::buffer() {
-        return buffer_;
-    }
-
-    void ByteStream::SetBuffer(std::vector<uint8_t> data) {
-        buffer_ = std::move(data);
-    }
-
-    void ByteStream::Show() const {
-        std::ostringstream oss;
-        oss << "ByteStream size = " << buffer_.size();
-        if (!buffer_.empty()) {
-            oss << ", first 8 bytes: ";
-            for (size_t i = 0; i < std::min(buffer_.size(), size_t(8)); ++i) {
-                oss << std::hex << std::setw(2) << std::setfill('0')
-                    << static_cast<int>(buffer_[i]) << " ";
-            }
+void ByteStream::Show() const {
+    std::ostringstream oss;
+    oss << "ByteStream size = " << size;
+    if (data && size > 0) {
+        oss << ", first 8 bytes: ";
+        for (size_t i = 0; i < std::min(size, size_t(8)); ++i) {
+            oss << std::hex << std::setw(2) << std::setfill('0')
+                << static_cast<int>(data[i]) << " ";
         }
-        std::cout << oss.str() << std::endl;
     }
+    std::cout << oss.str() << std::endl;
+}
 
 } // namespace dataProducts
